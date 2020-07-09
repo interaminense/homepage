f20f25cf47bcb2a8eb8113a12663bddb39746c8a
Adriano Interaminense <adriano.interaminense@gmail.com>
2020-07-09 01:16:16 -0300

    Add UI for home page

--- src/App.js
+++ src/App.js
@@ -1,3 +1,23 @@
-import React from 'react';
+import React from "react";
+import Layout from "./components/Layout";
+import Profile from "./components/Profile";
+import CardList from "./components/CardList";
+import SocialNetwork from "./components/SocialNetwork";
+import "./css/simple-grid-css.min.css";
 
-export default () => <div>hello</div>;
+export default () => (
+  <>
+    <Layout>
+      <Layout.Row>
+        <Layout.Column size={3}>
+          <Profile />
+        </Layout.Column>
+        <Layout.Column size={7}>
+          <CardList />
+        </Layout.Column>
+      </Layout.Row>
+    </Layout>
+
+    <SocialNetwork />
+  </>
+);
