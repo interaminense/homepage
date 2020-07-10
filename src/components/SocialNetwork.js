64f2932011fa388e45e8cd496b8005f731d47974
Adriano Interaminense <adriano.interaminense@gmail.com>
2020-07-09 23:44:30 -0300

    Add icons for social network

--- src/components/SocialNetwork.js
+++ src/components/SocialNetwork.js
@@ -1,20 +1,55 @@
 import React from "react";
+import {
+  faBehance,
+  faCodepen,
+  faGithub,
+  faInstagram,
+  faLinkedin,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const Item = { name: "urlName", url: "https://interaminense.github.io" };
-
-const SocialNetwork = [Item, Item, Item, Item, Item];
+const SocialNetwork = [
+  {
+    icon: faGithub,
+    url: "http://github.com/interaminense",
+  },
+  {
+    icon: faCodepen,
+    url: "http://codepen.io/interaminense",
+  },
+  {
+    icon: faBehance,
+    url: "http://behance.net/imcreator",
+  },
+  {
+    icon: faLinkedin,
+    url: "https://www.linkedin.com/in/adriano-interaminense-405a90b3/",
+  },
+  {
+    icon: faInstagram,
+    url: "http://twitter.com/adinteraminense",
+  },
+  {
+    icon: faTwitter,
+    url: "http://twitter.com/adinteraminense",
+  },
+];
 
 export default () => (
-  <ul className="SocialNetWork">
-    {SocialNetwork.map(({ name, url }, index) => (
+  <ul className="SocialNetwork">
+    {SocialNetwork.map(({ icon, url }, index) => (
       <li className="SocialNetwork__item" key={index}>
         <a
           className="SocialNetwork__link"
           href={url}
           rel="noopener noreferrer"
           target="_blank"
+          title={url}
         >
-          {name}
+          <div>
+            <FontAwesomeIcon icon={icon} />
+          </div>
         </a>
       </li>
     ))}
