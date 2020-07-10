e5f518c21fc36ef4a4db5740a2b0eb493dcc5a91
Adriano Interaminense <adriano.interaminense@gmail.com>
2020-07-09 23:46:25 -0300

    Update Card to use tags and remove btn read more

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,21 +1,27 @@
 import React from "react";
 
-export default ({ name, description, image, url }) => (
-  <div className="Card">
+export default ({ name, date, description, image, url, tags }) => (
+  <a className="Card" href={url} rel="noopener noreferrer" target="_blank">
     <div className="Card__header">
       <img src={image} alt="Card" />
     </div>
-    <h3 className="Card__name">{name}</h3>
-    <p className="Card__description">{description}</p>
-    {url && (
-      <a
-        className="Card__link"
-        href={url}
-        rel="noopener noreferrer"
-        target="_blank"
-      >
-        read more
-      </a>
-    )}
-  </div>
+
+    <div className="Card__body">
+      <h2 className="Card__name">{name}</h2>
+
+      <span>{date}</span>
+
+      <p className="Card__description">{description}</p>
+
+      {!!tags.length && (
+        <div className="Card__tags">
+          {tags.map((tag, index) => (
+            <span className="Card__tag" key={index}>
+              {tag}
+            </span>
+          ))}
+        </div>
+      )}
+    </div>
+  </a>
 );
--- src/components/CardList.js
+++ src/components/CardList.js
@@ -5,15 +5,45 @@ import Masonry from "react-masonry-component";
 const Project = {
   name: "My awesome project name",
   description: "My awesome project description",
-  image: "",
+  image: "http://www.interaminense.com/src/imgs/neon.jpg",
   url: "https://interaminense.github.io",
-  date: "12/10/2020",
+  date: "12 October, 2020",
+  tags: ["codepen", "reactjs"],
 };
 
-const Projects = [Project, Project, Project, Project, Project, Project];
+const Projects = [
+  {
+    ...Project,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia quisquam consectetur quod excepturi dolore adipisci mollitia",
+  },
+  {
+    ...Project,
+    description:
+      "Ipsa dignissimos, ut optio vero sequi, soluta tempore atque blanditiis harum cumque sit autem.",
+  },
+  Project,
+  Project,
+  {
+    ...Project,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia quisquam consectetur quod excepturi dolore adipisci mollitia",
+    image:
+      "http://localhost:8000/static/6d2f435689c8b89ab752139e547941c0/630fb/01.png",
+  },
+  Project,
+  Project,
+  Project,
+  Project,
+  Project,
+  Project,
+  Project,
+  Project,
+  Project,
+];
 
 export default () => (
-  <Masonry className="CardList" elementType={"ul"}>
+  <Masonry className="CardList" elementType="ul">
     {Projects.map((project, index) => (
       <li className="CardList__item" key={index}>
         <Card {...project} />
