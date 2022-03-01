c39a0ab36dacb1abb54515dee5f01bb168f2e04b
Adriano Interaminense <>
2022-03-01 01:40:48 -0300

    Remove masonry plugin and add it natively

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,14 +1,13 @@
 import React from "react";
 import Card from "./Card";
-import Masonry from "react-masonry-component";
 import { Projects } from "../utils/constants";
 
 export default () => (
-  <Masonry className="CardList" elementType="ul">
+  <ul className="CardList">
     {Projects.map((project, index) => (
       <li className="CardList__item" key={index}>
         <Card {...project} />
       </li>
     ))}
-  </Masonry>
+  </ul>
 );
